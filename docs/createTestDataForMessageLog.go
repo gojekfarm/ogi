91665c9df73ff99dcd94ee15a41a6c8bbe7c88f0
AbhishekKr <abhikumar163@gmail.com>
2018-04-12 20:14:14 +0530

    setup-kafka-docker and create test message-log for dev end-to-end test
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ docs/createTestDataForMessageLog.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	logger "github.com/gojekfarm/ogi/logger"
+	ogiproducer "github.com/gojekfarm/ogi/producer"
+)
+
+var (
+	msgMap = map[string][]string{
+		"nietzsche": []string{
+			"without music life would be a mistake",
+			"he who has a why to live, can bear almost any how",
+			"the doer alone learneth",
+		},
+		"bob-dylan": []string{
+			"no one is free, even the bords are chained in the sky",
+			"a mistake is to commit a misunderstanding",
+		},
+		"william-s-burrough": []string{
+			"nothing is true, everything is permitted",
+			"paranoid is someone who knows a little of what's going on",
+		},
+	}
+)
+
+func main() {
+	logger.SetupLogger()
+	k := ogiproducer.NewProducer()
+	for msgTopic, messages := range msgMap {
+		for idx, msg := range messages {
+			msg = fmt.Sprintf("%s,%d,%s", msgTopic, idx, msg)
+			ogiproducer.Produce(k, "testdata", []byte(msg), msgTopic)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	ogiconsumer "github.com/gojekfarm/ogi/consumer"
 	logger "github.com/gojekfarm/ogi/logger"
 )
 
 func main() {
 	logger.SetupLogger()
+	fmt.Println(`
+	   oooo          ggg      iiii
+	 ooo  ooo      gg  gg       ii
+	000    000       gggg       ii
+	 ooo  ooo           gg      ii
+	   oooo         gggg        ii
+	`)
 	ogiconsumer.Consume()
 }
